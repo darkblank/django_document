0a68d7ddc66150ec7a9579086d731574bfcfd94b
darkblank <darkblank1990@gmail.com>
2017-10-10 14:06:35 +0900

    재귀관계 실습

--- document/model/models.py
+++ document/model/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 # Create your models here.
+# choice 실습
 class Person(models.Model):
     SHIRT_SIZES = (
         ('S', 'Small'),
@@ -12,6 +13,7 @@ class Person(models.Model):
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
 
+# primary_key를 "id"가 아닌 다른 값으로 쓰고싶을 때
 class Fruit(models.Model):
     name = models.CharField(
         max_length=100,
@@ -19,10 +21,23 @@ class Fruit(models.Model):
     )
 
 
+# ForeignKey로 2개의 class를 연결
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Car(models.Model):
     name = models.CharField(max_length=50)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.name}'
+
+
+# 재귀관계 실습
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)

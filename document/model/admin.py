6d601620ad3fd0f65611bc0044e289b94130cc88
darkblank <darkblank1990@gmail.com>
2017-10-11 14:37:27 +0900

    one to one 실습

--- document/model/admin.py
+++ document/model/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from model.models import Car, Manufacturer, User, Pizza, Topping, FacebookUser, InstagramUser, Group, Idol, Membership
+from model.models import Car, Manufacturer, User, Pizza, Topping, FacebookUser, InstagramUser, Group, Idol, Membership, \
+    Place, Restaurant, Waiter
 
 admin.site.register(Car)
 admin.site.register(Manufacturer)
@@ -12,3 +13,6 @@ admin.site.register(InstagramUser)
 admin.site.register(Idol)
 admin.site.register(Group)
 admin.site.register(Membership)
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Waiter)

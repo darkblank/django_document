91c6810951bea047d1e9006d0eccc2b941050969
darkblank <darkblank1990@gmail.com>
2017-10-11 18:05:39 +0900

    inheritance proxy 실습

--- document/inheritance/admin.py
+++ document/inheritance/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from inheritance.models import Teacher, Student, School, Restaurant, Place
+from inheritance.models import Teacher, Student, School, Restaurant, Place, Champion
+
+
+class ChampionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'champion_type', 'rank',)
+    list_editable = ('rank',)
 
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(School)
 admin.site.register(Place)
 admin.site.register(Restaurant)
+admin.site.register(Champion, ChampionAdmin)
